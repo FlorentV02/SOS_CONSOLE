58e11994815169a282238296f6f3bd2f5ff39d98
FlorentV02 <Vieville.Florent03@gmail.com>
2022-06-12 22:19:49 +0200

    Suppression de fichier inutile
    supression des vieux fichier html

--- 404_error.php
+++ 404_error.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php require('includes/head.php'); ?>
+<link href="asset/css/contact.css" rel="stylesheet">
+
+
 </head>
 <body>
+    <div id="background-body">
+
+    <?php include('includes/nav-bar.php'); ?>
+
+
+    <section>
+
+
+        <div id="block-formulaire" class="my-5">
+
+          <p id="last" class="text-center title-page display-4 my-3 playfair">Dernière étape</p>
+          <p id="mail" class="text-center my-3 source-pro-sans">
+            Pour recevoir votre devis, vous devez indiqué une adresse email, le devis sera envoyé directement à l'adresse noté</p>
+
+          
+        
+          <form id="formulaire" class="source-pro-sans">
+          
+            <div class="mb-3 px-3 text-center w-100">
+                <label for="exampleInputEmail1" class="form-label">
+                  Indiqué votre adresse email <span class="text-danger font-weight-bold">*</span></label></label>
+                <input type="text" class="form-control form-style" id="mail" placeholder="Votre email"  aria-describedby="emailHelp">
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <button type="submit" name="envoyer" class="btn btn-search btn-outline-success playfair">Téléchargez</button>
+            </div>
+          </form>
+          
+          <p class="text-danger text-right">Champs obligatoire *</p>
+
+    </div>
+        
+        </div>
+    </section>
     
+    <?php require('includes/footer.php'); ?>
 </body>
 </html>
